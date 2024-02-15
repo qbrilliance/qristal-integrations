19e4b3d2bc45f255701507e394c1d075dc80ba5f
Johannes Kuhlmann <johannes.kuhlmann@quantum-brilliance.com>
2024-02-15 00:08:04 +0000

    bump image tags for infiniband and cereal

--- docker/README.md
+++
@@ -1,2 +0,0 @@
-## Docker resources
-This directory holds all items that relate to building Docker containers for ease of use with supported environments.
--- docker/qristal/entry-qb-all.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash    
-
-# Jupyter Lab: serve at port 8889
-jupyter lab --allow-root --no-browser --notebook-dir=$QB_SDK_ROOT --ip=0.0.0.0 --port=8889 --ServerApp.token="" --ServerApp.terminado_settings ="{'shell_command': ['/bin/bash']}" --IPKernelApp.pylab="inline"
